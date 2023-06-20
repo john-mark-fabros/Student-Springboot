e067fa56b842279ced6eb4e7a595a852cc28039a
John Mark Fabros <jmaking12345@gmail.com>
2023-06-20 14:17:14 +0800

    feat: add student controller

---
+++ src/main/java/com/example/Student/student/StudentController.java
@@ -0,0 +1,17 @@
+package com.example.Student.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<String> getStudent() {
+        return List.of("john mark", "mark john");
+    }
+}
